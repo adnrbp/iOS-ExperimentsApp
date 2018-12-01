cecdcdf905020bc534d4e3b56b52874c990dea4d
adnrbp <adnrbp@users.noreply.github.com>
2018-12-01 14:34:54 -0500

    Initial Commit

---
+++ ExperimentsApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExperimentsApp
+//
+//  Created by Ghosty on 12/1/18.
+//  Copyright © 2018 componos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExperimentsApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExperimentsApp
+//
+//  Created by Ghosty on 12/1/18.
+//  Copyright © 2018 componos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
